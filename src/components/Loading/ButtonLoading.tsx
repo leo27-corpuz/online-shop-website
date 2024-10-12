f2da6924b0e1d0ae0e3c10f9d5d62c64302b60c6
leo27-corpuz <leocorpuz.jr27@gmail.com>
2024-10-12 16:46:21 +0800

    subscribe

---
+++ src/components/Loading/ButtonLoading.tsx
@@ -0,0 +1,12 @@
+
+'use client'
+import React from "react"
+import '@/styles/buttonLoading.scss'
+const ButtonLoading: React.FC = () => {
+    return(
+        <>
+           <span className="loader"></span>
+        </>
+    ) 
+}
+export default ButtonLoading
