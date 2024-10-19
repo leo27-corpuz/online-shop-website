24b93b7489c87b90566855357ed4af875e38752a
leo27-corpuz <leocorpuz.jr27@gmail.com>
2024-10-19 20:19:53 +0800

    fixed deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,61 +1,60 @@
 'use client'
-import React from "react"
-// import React, {useEffect, useState } from "react"
-// import dynamic from 'next/dynamic'
-// import BannerLoading from "@/components/Loading/BannerLoadingSkeleton";
-import { robotoRegular } from "@/fonts/font";
-// const CarouselBanner = dynamic(() => import('@/components/Banner/carousel-banner'), { 
-// 	ssr: true,
-// 	loading: () => <BannerLoading />
-// })
-// const CarouselBrands = dynamic(() => import('@/components/Brands/carousel-brands'), {
-// 	ssr: true,
-// 	loading: () => <p>Loading top brands...</p>
-// })
-// const CarouselProducts = dynamic(() => import('@/components/Product/carousel-products'), {
-// 	ssr: true,
-// 	loading: () => <p>Loading featured products...</p>
-// })
-// import NewslettterComponents from "@/components/Newsletter/NewsletterComponent";
-// import { FeaturedProduct } from "@/fixtures/featured-product"
-// import { topBrands } from "@/fixtures/top-brands"
-// import { Banner } from "@/fixtures/banner"
-// import { BannerContext } from "@/contexts/banner"
-// import { TopBrandsContext } from "@/contexts/top-brands"
-// import { FeaturedProductContext } from "@/contexts/featured-product";
+import React, {useEffect, useState } from "react"
+import dynamic from 'next/dynamic'
+import BannerLoading from "@/components/Loading/BannerLoadingSkeleton";
+import { robotoRegular, robotoRegularBold } from "@/fonts/font";
+const CarouselBanner = dynamic(() => import('@/components/Banner/carousel-banner'), { 
+	ssr: true,
+	loading: () => <BannerLoading />
+})
+const CarouselBrands = dynamic(() => import('@/components/Brands/carousel-brands'), {
+	ssr: true,
+	loading: () => <p>Loading top brands...</p>
+})
+const CarouselProducts = dynamic(() => import('@/components/Product/carousel-products'), {
+	ssr: true,
+	loading: () => <p>Loading featured products...</p>
+})
+import NewslettterComponents from "@/components/Newsletter/NewsletterComponent";
+import { FeaturedProduct } from "@/fixtures/featured-product"
+import { topBrands } from "@/fixtures/top-brands"
+import { Banner } from "@/fixtures/banner"
+import { BannerContext } from "@/contexts/banner"
+import { TopBrandsContext } from "@/contexts/top-brands"
+import { FeaturedProductContext } from "@/contexts/featured-product";
 const Home: React.FC = () => {
-	// //top brands
-	// interface imageInterace {
-// 		url: string,
-// 		alt: string
-// 	}
-// 	interface brandsInterface {
-// 		img: imageInterace,
-// 		title: string
-// 		url: string
-// 	}
-// 	const [brands, setBrands] = useState<brandsInterface[]>([])
-// 	useEffect(() => {
-// 		setBrands(topBrands)
-// 	}, [])
+	//top brands
+	interface imageInterace {
+		url: string,
+		alt: string
+	}
+	interface brandsInterface {
+		img: imageInterace,
+		title: string
+		url: string
+	}
+	const [brands, setBrands] = useState<brandsInterface[]>([])
+	useEffect(() => {
+		setBrands(topBrands)
+	}, [])
 
-//   //feature products
-//   	interface productsInterface{
-// 		title: string,
-// 		img: string,
-// 		isSale?: boolean, 
-// 		originalPrice?: number | string,
-// 		price: number | string,
-// 		id: number | string,
-// 		url: string
-// 	}
-// 	const [products, setProducts] = useState<productsInterface[]>([])
-// 	useEffect(() => {
-// 		setProducts(FeaturedProduct)
-// 	}, [])
+  //feature products
+  	interface productsInterface{
+		title: string,
+		img: string,
+		isSale?: boolean, 
+		originalPrice?: number | string,
+		price: number | string,
+		id: number | string,
+		url: string
+	}
+	const [products, setProducts] = useState<productsInterface[]>([])
+	useEffect(() => {
+		setProducts(FeaturedProduct)
+	}, [])
     return(
         <>
-			{/* <section className="w-full mx-auto banner-section lg:container lg:px-4">
+			<section className="w-full mx-auto banner-section lg:container lg:px-4">
 				<BannerContext.Provider value={Banner}>
 					<CarouselBanner />
 				</BannerContext.Provider>
@@ -64,7 +63,7 @@ const Home: React.FC = () => {
 				<div className="container mx-auto">
 					<div className="mb-8 lg:mb-12 animation-initial-to-fade">
 						<h1 className={`text-2xl text-center tracking text-primaryTextColor font-mono-regular-semibold  md:text-3xl lg:text-4xl`}>Transform Your Everyday Look with the Timeless Elegance</h1>
-						<p className={"text-center mt-2 text-sm lg:w-2/4 mx-auto " + robotoRegular.className }>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+						<p className={"text-center mt-2 text-sm lg:w-2/4 mx-auto " + robotoRegular.className }>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s</p>
 					</div>
 					<div className="about-products grid md:grid-cols-3">
 						<div className="heart w-full px-4 py-6 bg-[#fcf2e6] min-h-48 md:min-h-64 lg:min-h-72 lg:px-8 lg:py-4 flex items-center animation-initial-to-fade">
@@ -95,7 +94,7 @@ const Home: React.FC = () => {
 				<div className="container mx-auto">
 					<div className="mb-8 lg:mb-12 animation-initial-to-fade">
 						<h1 className={"text-2xl tracking text-primaryTextColor text-center md:text-3xl lg:text-4xl " + robotoRegularBold.className }>Top Brands</h1>
-						<p className={"text-center mt-2 text-sm lg:w-2/4 mx-auto " + robotoRegular.className }>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+						<p className={"text-center mt-2 text-sm lg:w-2/4 mx-auto " + robotoRegular.className }>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s</p>
 					</div>
 					<div className="animation-initial-to-fade">
 						<TopBrandsContext.Provider value={brands}>
@@ -120,8 +119,7 @@ const Home: React.FC = () => {
 			<section className="newsletter-container container w-full mt-16 md:mt-24 px-4 mx-auto">
 				<NewslettterComponents />
 			</section>
-			<section className="mb-24 sm:hidden"></section> */}
-			<p className={"text-center mt-2 text-sm lg:w-2/4 mx-auto " + robotoRegular.className }>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s</p>
+			<section className="mb-24 sm:hidden"></section>
         </>
     ) 
 }
