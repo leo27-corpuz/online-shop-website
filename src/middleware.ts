1efcc3f52efa163372f7d4885d81cb0194e1f8d9
leo27-corpuz <leocorpuz.jr27@gmail.com>
2024-10-20 10:28:04 +0800

    csp

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,7 +17,7 @@ export function middleware(request: NextRequest) {
   `
   const cspHeader = `
     default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' ${cspScrit};
+    script-src 'self' 'unsafe-eval' ${cspScrit};
     style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data: ${cspImages};
     font-src 'self';
