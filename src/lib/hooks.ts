19fa02c042e04596eb4def3b1aa9a751b35a9cf2
leo27-corpuz <leocorpuz.jr27@gmail.com>
2024-11-05 09:38:38 +0800

    fixed dev deployment

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -2,5 +2,5 @@ import { useDispatch, useSelector, useStore } from 'react-redux';
 import type { AppDispatch, AppStore, RootState } from './store';
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: (selector: (state: RootState) => any) => any = useSelector;
+export const useAppSelector = <TSelected>(selector: (state: RootState) => TSelected): TSelected =>  useSelector(selector);
 export const useAppStore = () => useStore<AppStore>();
